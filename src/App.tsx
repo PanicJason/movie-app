4e7583a5e42df068476e387ae9a64336ebf4e1d9
PanicJason <skyhumteok@naver.com>
2023-09-01 17:57:30 +0900

    feat: header set

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,17 @@ import { RecoilRoot } from 'recoil';
 import Router from './Router';
 import { QueryClient, QueryClientProvider } from 'react-query';
 
+// App 컴포넌트 정의
 const App : FC = () => {
 
+  // QueryClient를 생성하고 설정하는 부분
   const [queryClient] = useState(
     ()=>
       new QueryClient ({
         defaultOptions :{
           queries :{
-            refetchOnWindowFocus : false,
-            retry :0
+            refetchOnWindowFocus : false,   // 창이 포커스를 잃었을 때 쿼리 자동 재요청 비활성화
+            retry :0                        // 쿼리 실패 시 재시도 횟수
           },
         },
       })
@@ -21,9 +23,9 @@ const App : FC = () => {
 
   return (
     <RecoilRoot>
-      <QueryClientProvider client = {queryClient}>
+      <QueryClientProvider client = {queryClient}>  {/* QueryClientProvider로 QueryClient를 애플리케이션에 제공 */}
         <BrowserRouter>
-          <Router/>
+          <Router/>                                 {/* 라우터 컴포넌트를 사용하여 애플리케이션 라우팅 설정 */}
         </BrowserRouter>
       </QueryClientProvider>
     </RecoilRoot>
