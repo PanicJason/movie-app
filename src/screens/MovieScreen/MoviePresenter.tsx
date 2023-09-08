2452d021d498f16159af14de21efe234423219db
PanicJason <skyhumteok@naver.com>
2023-09-08 14:02:24 +0900

    feat: section

--- src/screens/MovieScreen/MoviePresenter.tsx
+++ src/screens/MovieScreen/MoviePresenter.tsx
@@ -1,9 +1,44 @@
-const MoviePresenter : React.FC<any>=({
+import Loader from "../../components/Loader/Loder";
 
+interface MoviePresenterProps{
+    nowPlaying: any[] | null;
+    upcoming: any[] | null;
+    popular: any[] | null;
+    topRated: any[] | null;
+    error: string | null;
+    loading: boolean;
+    popularInfinite: any[] | null;
+    nowPlayingInfinite: any[] | null;
+    upcomingInfinite: any[] | null;
+    topRatedInfinite: any[] | null;
+}
+
+const MoviePresenter : React.FC<MoviePresenterProps>=({
+    nowPlaying,
+    upcoming,
+    popular,
+    topRated,
+    error,
+
+    loading,
+
+    nowPlayingInfinite,
+    popularInfinite,
+    upcomingInfinite,
+    topRatedInfinite,
 })=>{
-    return (
-        <div>무비 앱임</div>
-    );
+
+    const {
+        location: { pathname },} = window;
+
+    console.log(nowPlaying);
+
+    return loading ? (
+            <Loader/>
+        ) :
+        (
+            <div>무비 앱임</div>
+        );
 }
 
 export default MoviePresenter;
