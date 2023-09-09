e42163561478cb26f394b883c3538785bcd7f119
PanicJason <skyhumteok@naver.com>
2023-09-09 11:29:54 +0900

    feat: detail component logic

--- src/Router.tsx
+++ src/Router.tsx
@@ -3,6 +3,7 @@ import { Route, Routes, useLocation } from "react-router-dom";
 import HomeContainer from './screens/HomeScreen';
 import ScreenHeader from "./components/Header/ScreenHeader";
 import MovieContainer from "./screens/MovieScreen";
+import DetailContainer from "./screens/DetailScreen";
 
 
 
@@ -17,6 +18,7 @@ const Router : FC = (props) => {
             <Routes>
                 <Route path="/" Component={HomeContainer}/>
                 <Route path="/movie/*" Component={MovieContainer}/>
+                <Route path="/detail/:id" Component={DetailContainer}/>
             </Routes>
         </div>
     )
--- src/screens/DetailScreen/DetailContainer.tsx
+++ src/screens/DetailScreen/DetailContainer.tsx
@@ -1,12 +1,38 @@
 import { Component } from "react";
 import DetailPresenter from "./DetailPresenter";
+import { useParams } from "react-router-dom";
+import withRouter from "../../utils/withRouter";
 
-
+interface DetailContainerState{
+    result : any | null,
+    error : string | null,
+    loading : boolean,
+    recommendations : any,
+    cast : any,
+    keyword : any,
+    backdrops : any,
+    posters : any,
+    tvDetail2 : any,
+    reviews : any
+}
 
 class DetailContainer extends Component<{}, any> {
     constructor(props : any){
         super(props);
+        this.state={
+            result : [],
+            error : null,
+            loading : true,
+            recommendations : [null],
+            cast : [],
+            keyword : [],
+            reviews : [],
+            backdrops : [],
+            posters : [],
+            tvDetail2 : []
+        };
     }
+
 
 
     render() {
@@ -15,4 +41,4 @@ class DetailContainer extends Component<{}, any> {
 
 }
 
-export default DetailContainer;
+export default withRouter(DetailContainer);
