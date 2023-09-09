b791988b4bea8bfa0ac919b0f810fd72d4a4b9e2
PanicJason <skyhumteok@naver.com>
2023-09-09 11:05:41 +0900

    feat: create Detail Component

---
+++ src/screens/DetailScreen/DetailContainer.tsx
@@ -0,0 +1,18 @@
+import { Component } from "react";
+import DetailPresenter from "./DetailPresenter";
+
+
+
+class DetailContainer extends Component<{}, any> {
+    constructor(props : any){
+        super(props);
+    }
+
+
+    render() {
+        return <DetailPresenter />
+    }
+
+}
+
+export default DetailContainer;
